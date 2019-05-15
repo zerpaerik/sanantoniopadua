e7900167845e6519e6ef6a017d5087cc6a08bdc4
Diego <diegoarmando957@hotmail.com>
2019-05-14 21:24:24 -0400

    Edit atenciones

--- resources/views/movimientos/atenciones/edit.blade.php
+++ resources/views/movimientos/atenciones/edit.blade.php
@@ -4,35 +4,35 @@
 
 <br>
 <div class="row">
-	<div class="col-xs-12">
-		<div class="box">
-			<div class="box-header">
-				<div class="box-name">
-					<i class="fa fa-users"></i>
-					<span><strong>Actualizar Atencion</strong></span>
-				</div>
-				<div class="box-icons">
-					<a class="collapse-link">
-						<i class="fa fa-chevron-up"></i>
-					</a>
-					<a class="expand-link">
-						<i class="fa fa-expand"></i>
-					</a>
-				</div>
-				<div class="no-move"></div>
-			</div>
-			<div class="box-content">
-				<h4 class="page-header"></h4>
-				<form class="form-horizontal" role="form" method="post" action="atenciones/edit/{{$atencion->id}}">
-					{{ csrf_field() }}
-					<div class="form-group">
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-header">
+        <div class="box-name">
+          <i class="fa fa-users"></i>
+          <span><strong>Actualizar Atencion</strong></span>
+        </div>
+        <div class="box-icons">
+          <a class="collapse-link">
+            <i class="fa fa-chevron-up"></i>
+          </a>
+          <a class="expand-link">
+            <i class="fa fa-expand"></i>
+          </a>
+        </div>
+        <div class="no-move"></div>
+      </div>
+      <div class="box-content">
+        <h4 class="page-header"></h4>
+        <form class="form-horizontal" role="form" method="post" action="atenciones/edit/{{$atencion->id}}">
+          {{ csrf_field() }}
+          <div class="form-group">
 
-						<div class="row">
+            <div class="row">
 
-						<label class="col-sm-1 control-label">Pacientes</label>
-						<div class="col-sm-3">
-							<select id="el1" name="id_paciente">
-								@foreach($pacientes as $pac)
+            <label class="col-sm-1 control-label">Pacientes</label>
+            <div class="col-sm-3">
+              <select id="el1" name="id_paciente">
+                @foreach($pacientes as $pac)
                   @if($atencion->paciente->id == $pac->id)
                     <option value="{{$pac->id}}" selected="selected">
                       {{$pac->nombres}} {{$pac->apellidos}}-{{$pac->dni}}
@@ -42,36 +42,44 @@
                       {{$pac->nombres}} {{$pac->apellidos}}-{{$pac->dni}}
                     </option>
                   @endif
-								@endforeach
-							</select>
-						</div>
+                @endforeach
+              </select>
+            </div>
 
-						</div>
-						<br>
-						<div class="row">
-						<label class="col-sm-1 control-label">Origen</label>
-						<div class="col-sm-3">
-							<select id="el2" name="origen">
-								  <option value="0">Seleccione el Origen</option>
+            </div>
+            <br>
+            <div class="row">
+            <label class="col-sm-1 control-label">Origen</label>
+            <div class="col-sm-3">
+              <select id="el2" name="origen">
+                  <option value="0">Seleccione el Origen</option>
                   @if($atencion->origen == 1)
                     <option value="1" selected="selected">Personal</option>
                     <option value="2">Profesional</option>
-                  @else
+                    <option value="3">Particular</option>
+                  @elseif($atencion->origen == 2)
                     <option value="1">Personal</option>
                     <option value="2" selected="selected">Profesional</option>
+                    <option value="3">Particular</option>
+                  @else
+                  <option value="1">Personal</option>
+                  <option value="2">Profesional</option>
+                  <option value="3" selected="selected">Particular</option>
                   @endif
-									
-							</select>
-						</div>
+                  
+              </select>
+            </div>
 
-						<div class="col-sm-7">
-							  <div id="origen1">
+            <div class="col-sm-7">
+                <div id="origen1">
                 @if($atencion->origen == 1)
                   <label class="col-sm-3 control-label">Personal</label>
                   
-                @else
+                @elseif($atencion->origen == 2)
                   <label class="col-sm-3 control-label">Profesional</label>
-                
+                @else
+
+                <label class="col-sm-3 control-label">Particular</label>
                 @endif
                   <div class="col-sm-6">
                     <select id="el4" name="origen_usuario">
@@ -89,10 +97,10 @@
                     </select>
                   </div>
                 </div>
-						</div>
-					</div>
-					<br>
-					
+            </div>
+          </div>
+          <br>
+          
           @if($atencion->es_servicio)
             <div class="row">
               <label class="col-sm-12 alert"><i class="fa fa-tasks" aria-hidden="true"></i> Servicios seleccionados</label>
@@ -173,7 +181,7 @@
               <!-- /sheepIt Form --> 
             </div>
           @endif
-					
+          
           <hr>
           <div class="form-group form-inline">
             <div class="col-sm-8 col-sm-offset-7">
@@ -219,15 +227,15 @@
               <input type="text" name="total_g" class="number form-control" value="{{ $atencion->pendiente}}" id="total_g" readonly="readonly" style="width: 150px">
             </div>
           </div>
-	
-					<br>
-					<input type="button" onclick="form.submit()" style="margin-left:15px; margin-top: 20px;" class="col-sm-2 btn btn-primary" value="Agregar">
+  
+          <br>
+          <input type="button" onclick="form.submit()" style="margin-left:15px; margin-top: 20px;" class="col-sm-2 btn btn-primary" value="Agregar">
 
-					<a href="{{route('atenciones.index')}}" style="margin-left:15px; margin-top: 20px;" class="col-sm-2 btn btn-danger">Volver</a>
-				</form>	
-			</div>
-		</div>
-	</div>
+          <a href="{{route('atenciones.index')}}" style="margin-left:15px; margin-top: 20px;" class="col-sm-2 btn btn-danger">Volver</a>
+        </form> 
+      </div>
+    </div>
+  </div>
 </div>
 @section('scripts')
 
@@ -352,31 +360,31 @@ function calculo_general() {
 
 // Run Select2 on element
 function Select2Test(){
-	$("#el2").select2();
-	$("#el1").select2();
-	$("#el3").select2();
+  $("#el2").select2();
+  $("#el1").select2();
+  $("#el3").select2();
   $("#el5").select2();
   $("#el4").select2();
 }
 $(document).ready(function() {
-	// Load script of Select2 and run this
-	LoadSelect2Script(Select2Test);
-	LoadTimePickerScript(DemoTimePicker);
-	WinMove();
+  // Load script of Select2 and run this
+  LoadSelect2Script(Select2Test);
+  LoadTimePickerScript(DemoTimePicker);
+  WinMove();
 });
 
 function DemoTimePicker(){
-	$('#input_date').datepicker({
-	setDate: new Date(),
-	minDate: 0});
-	$('#input_time').timepicker({
-		setDate: new Date(),
-		stepMinute: 10
-	});
-	$('#input_time2').timepicker({
-		setDate: new Date(),
-		stepMinute: 10
-	});
+  $('#input_date').datepicker({
+  setDate: new Date(),
+  minDate: 0});
+  $('#input_time').timepicker({
+    setDate: new Date(),
+    stepMinute: 10
+  });
+  $('#input_time2').timepicker({
+    setDate: new Date(),
+    stepMinute: 10
+  });
 }
 </script>
 
@@ -386,9 +394,11 @@ function DemoTimePicker(){
           var link;
           if ($(this).val() ==  1) {
             link = '/movimientos/atencion/personal/';
-          }else{
+          }else if ($(this).val() ==  2){
             link = '/movimientos/atencion/profesional/';
-          }
+          } else {
+        link = '/movimientos/atencion/particular/';
+      }
 
           $.ajax({
                  type: "get",
@@ -406,7 +416,7 @@ function DemoTimePicker(){
     </script>
 
     <script>
-    	    function add_li()
+          function add_li()
         {
             var nuevoLi=document.getElementById("el3").value;
             if(nuevoLi.length>0)
